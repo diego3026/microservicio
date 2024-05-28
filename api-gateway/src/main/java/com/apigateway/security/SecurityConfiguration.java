d1f77e2caa1529b6fabc9b56ad68af673dfe849b
diego oñate <diegoonate3026@gmail.com>
2024-05-28 12:55:31 -0500

    implementando seguridad

--- api-gateway/src/main/java/com/apigateway/security/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/apigateway/security/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.util.function.Consumer;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,8 +19,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-//    @Value("${okta.oauth2.audience}")
-//    private String audience;
+    @Value("${okta.oauth2.audience}")
+    private String audience;
 
     private final ClientRegistrationRepository clientRegistrationRepository;
 
@@ -29,28 +31,43 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/","/images/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(authorization -> authorization
+                                .authorizationRequestResolver(
+                                        authorizationRequestResolver(this.clientRegistrationRepository)
+                                )
+                        )
                 )
-                .oauth2Login(withDefaults());
+                .oauth2ResourceServer(jwt -> jwt.jwt(withDefaults()))
+                .logout(logout -> logout.logoutSuccessHandler(logoutSuccessHandler()));
         return http.build();
     }
-//
-//    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
-//            ClientRegistrationRepository clientRegistrationRepository) {
-//
-//        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
-//                new DefaultOAuth2AuthorizationRequestResolver(
-//                        clientRegistrationRepository, "/oauth2/authorization");
-//        authorizationRequestResolver.setAuthorizationRequestCustomizer(
-//                authorizationRequestCustomizer());
-//
-//        return authorizationRequestResolver;
-//    }
 
-//    private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
-//        return customizer -> customizer
-//                .additionalParameters(params -> params.put("audience", audience));
-//    }
+    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
+            ClientRegistrationRepository clientRegistrationRepository) {
+
+        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(
+                        clientRegistrationRepository, "/oauth2/authorization");
+        authorizationRequestResolver.setAuthorizationRequestCustomizer(
+                authorizationRequestCustomizer());
+
+        return authorizationRequestResolver;
+    }
+
+    private Consumer<OAuth2AuthorizationRequest.Builder>
+    authorizationRequestCustomizer() {
+        return customizer -> customizer
+                .additionalParameters(params -> params.put("audience", audience));
+    }
+
+    private LogoutSuccessHandler logoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+        return logoutSuccessHandler;
+    }
 }
